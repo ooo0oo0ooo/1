f4ea9b2ca9d6155eb03631361ce33b6d0da2ba78
ooo0oo0ooo <118867034+ooo0oo0ooo@users.noreply.github.com>
2022-12-02 19:19:53 +0800

    Create vue.sh

---
+++ vue.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git clone https://github.com/lyzs90/vuewp.git && cd vuewp
+npm install && docker-compose up -d
+docker-machine ip && cp .env.example  .env
+echo "
+  运行 http://container-ip:8000 进行安装;http://localhost:8080 浏览;
+  为了获得更好的开发体验，您应该运行 npm run dev 来运行Browsersync代理服务器;
+  运行生产构建  npm run prod
+  使用zeit/serve等http服务器进行预览，例如。serve wp-content/themes/vuewp/dist
+  要用作主题，只需将vuewp目录复制到您的Wordpress主题，即/var/www/html/wp-content/themes
+  对于无头设置，一个选项是将S3上的dist目录托管为静态站点。
+  “
