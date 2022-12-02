4cae4e193d2f14798da7e19b8e1b6597525ef179
ooo0oo0ooo <118867034+ooo0oo0ooo@users.noreply.github.com>
2022-12-02 19:01:02 +0800

    Update dc.sh

--- dc.sh
+++ dc.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 #开始
-echo -n "国外请按1,国内请按2,一键安装请按3,所以你的选择是"
-read shuzi
-case  "$shuzi" in
-  1 )
-    echo "开始安装docker..."
-    wget -qO- get.docker.com | bash
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-    echo "安装完成,docker版本是"
-    echo "-------------------"
-    docker -v  && docker-compose --version
-    echo "设置开机自动启动及IP6"
-    echo "复制下列信息并粘贴...."
-    echo "
+echo -n "更新系统"
+sudo apy update && sudo apt upgrade
+sudo apt install wget git vim 
+echo "开始安装docker..."
+wget -qO- get.docker.com | bash
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo "安装完成,docker版本是"
+echo "-------------------"
+docker -v  && docker-compose --version
+echo "设置开机自动启动及IP6"
+echo "复制下列信息并粘贴...."
+echo "
          -----------------------------
       {
           "log-driver": "json-file",
@@ -30,10 +29,3 @@ case  "$shuzi" in
           -----------------------------"
     systemctl enable docker  && vim etc/docker/daemon.json
     systemctl restart docker || echo "安装失败！" 1>&2
-  ;;
-  2 )
-    echo "开始安装docker..."
-  ;;
-  3 )
-    chmod +x d.sh && ./d.sh
-  ;;
